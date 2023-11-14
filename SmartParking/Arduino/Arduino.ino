03daaf505b19a12b468d3071dfaeb556cee60bb1
SaraRoshanZamir <SaraRoshanZamir@outlook.com>
2023-11-14 22:24:47 +0330

    smart parking final, arduino and mcu

--- SmartParking/Arduino/Arduino.ino
+++ SmartParking/Arduino/Arduino.ino
@@ -1,22 +1,51 @@
 //arduino uno
 #include <Servo.h>
-Servo myservo; 
-Servo myservo1;
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+byte readCard[4];
+String MasterTag = "D3FEFCF6";
+String tagID = "";
+
+Servo myservo;
+MFRC522 mfrc522(SS_PIN, RST_PIN);
 int pos = 0;
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin = 3;
+const int echoPin = 5;
 long duration;
 int distance;
+
 void setup() {
-  myservo.attach(5);
-  myservo1.attach(6);
+  myservo.attach(6);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT); 
+  SPI.begin();
   Serial.begin(9600); 
+  mfrc522.PCD_Init();
+  delay(4);
+  mfrc522.PCD_DumpVersionToSerial();
+  Serial.println(" Access Control ");
+  Serial.println("Scan Your Card>>");
+}
+
+void handle_moveServo(){
+  for (pos = 0; pos <= 90; pos += 1) {
+    myservo.write(pos);            
+    delay(15);                     
+
+  }
+    delay(1000);   
+  for (pos = 90; pos >= 0; pos -= 1) { 
+    myservo.write(pos);             
+    delay(15);                   
+  }
 }
 
 void loop() {
-   digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
@@ -24,34 +53,47 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
   if(distance<10){
-     for (pos = 0; pos <= 180; pos += 1) {
-    myservo.write(pos);            
-    delay(15);                     
-
+    handle_moveServo();
   }
 
-  for (pos = 180; pos >= 0; pos -= 1) { 
-    myservo.write(pos);             
-    delay(15);                   
-  }
-  }
-  //  if(distance>20){
-  //    for (pos = 0; pos <= 180; pos += 1) {
-  //   myservo1.write(pos);            
-  //   delay(15);                     
-
-  // }
-
-  // for (pos = 180; pos >= 0; pos -= 1) { 
-  //   myservo1.write(pos);             
-  //   delay(15);                   
-  // }
-  // }
-
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  Serial.println("hello");
   delay(3000);
+
+   while (getID()) {
+  
+    if (tagID == MasterTag){
+      Serial.println(" Access Granted!");
+      Serial.println("--------------------------");
+      handle_moveServo();
+
+    }
+
+    else{
+      Serial.println(" Access Denied!");
+      Serial.println("--------------------------");
+    }
+
+    delay(2000);
+    Serial.println(" Access Control ");
+    Serial.println("Scan Your Card>>");
+  }
 }
 
+boolean getID() 
+{
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return false;
+  }
+
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+  return false;
+  }
+  tagID = "";
+  for ( uint8_t i = 0; i < 4; i++) {
+  tagID.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  
+  tagID.toUpperCase();
+  mfrc522.PICC_HaltA(); // Stop reading
+  return true;
+
+}
